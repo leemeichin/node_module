2830f4eacae93eefc9bd2e222a46fcd5777461c1
Lee Machin <lee@new-bamboo.co.uk>
2013-08-17 13:02:24 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in node_module.gemspec
+gemspec
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'minitest/spec'
+require 'minitest/mock'
+require 'minitest/pride'
+require 'minitest/autorun'
+
+require File.expand_path('../lib/node_module.rb', __FILE__)
